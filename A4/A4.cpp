b7eadb943cbf70b627ab57781aafa058fe10f661
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2017-06-21 05:28:04 -0400

    Add progress bar

--- A4/A4.cpp
+++ A4/A4.cpp
@@ -147,13 +147,14 @@ void A4_Render(
 
   double Infinity = std::numeric_limits<double>::infinity();
 
-  uint ssFactor = 2;
+  uint ssFactor = 3;
 
   // for (unsigned int y = 241; y < 242; y += 1) {
   //   for (unsigned int x = 217; x < 218; x += 1) {
   for (double y = 0; y < ny; y += 1) {
     for (double x = 0; x < nx; x += 1) {
-      std::cout << "Progress: " << (y * ny + x) / (1.0 * nx * ny) << std::endl;
+      printf("\r[%3d%%]", (int) ((y * ny + x) * 100 / (1.0 * nx * ny)));
+
       // if (!(y == 231 && x == 140) && !(y == 231 && x == 141)) continue;
 
       // std::cerr << "x: " << x << std::endl;
@@ -318,6 +319,8 @@ void A4_Render(
       image(x, y, 2) = fragColourSuperSample.b;
     }
   }
+
+  printf("\n");
 
   std::cerr << "Misses: " << miss << std::endl;
   std::cerr << "Hits: " << hit << std::endl;
