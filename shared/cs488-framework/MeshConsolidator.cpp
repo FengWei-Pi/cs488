084f1b736a69a155f84f5644bacab8feba3ab700
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2017-07-15 03:11:13 -0400

    Replace tabs with spaces in shared/

--- shared/cs488-framework/MeshConsolidator.cpp
+++ shared/cs488-framework/MeshConsolidator.cpp
@@ -23,68 +23,68 @@ MeshConsolidator::~MeshConsolidator()
 //----------------------------------------------------------------------------------------
 template <typename T>
 static void appendVector (
-		std::vector<T> & dest,
-		const std::vector<T> & source
+    std::vector<T> & dest,
+    const std::vector<T> & source
 ) {
-	// Increase capacity to hold source.size() more elements
-	dest.reserve(dest.size() + source.size());
+  // Increase capacity to hold source.size() more elements
+  dest.reserve(dest.size() + source.size());
 
-	dest.insert(dest.end(), source.begin(), source.end());
+  dest.insert(dest.end(), source.begin(), source.end());
 }
 
 
 //----------------------------------------------------------------------------------------
 MeshConsolidator::MeshConsolidator(
-		std::initializer_list<ObjFilePath> objFileList
+    std::initializer_list<ObjFilePath> objFileList
 ) {
 
-	MeshId meshId;
-	vector<vec3> positions;
-	vector<vec3> normals;
+  MeshId meshId;
+  vector<vec3> positions;
+  vector<vec3> normals;
   vector<vec2> uvCoords;
-	BatchInfo batchInfo;
-	unsigned long indexOffset(0);
+  BatchInfo batchInfo;
+  unsigned long indexOffset(0);
 
     for(const ObjFilePath & objFile : objFileList) {
-	    ObjFileDecoder::decode(objFile.c_str(), meshId, positions, normals, uvCoords);
+      ObjFileDecoder::decode(objFile.c_str(), meshId, positions, normals, uvCoords);
 
-	    uint numIndices = positions.size();
+      uint numIndices = positions.size();
 
-	    if (numIndices != normals.size()) {
-		    throw Exception("Error within MeshConsolidator: "
-					"positions.size() != normals.size()\n");
-	    }
+      if (numIndices != normals.size()) {
+        throw Exception("Error within MeshConsolidator: "
+          "positions.size() != normals.size()\n");
+      }
 
       if (numIndices != uvCoords.size()) {
-		    throw Exception("Error within MeshConsolidator: "
-					"positions.size() != uvCoords.size()\n");
-	    }
+        throw Exception("Error within MeshConsolidator: "
+          "positions.size() != uvCoords.size()\n");
+      }
 
-	    batchInfo.startIndex = indexOffset;
-	    batchInfo.numIndices = numIndices;
+      batchInfo.startIndex = indexOffset;
+      batchInfo.numIndices = numIndices;
 
-	    m_batchInfoMap[meshId] = batchInfo;
+      m_batchInfoMap[meshId] = batchInfo;
 
-	    appendVector(m_vertexPositionData, positions);
-	    appendVector(m_vertexNormalData, normals);
+      appendVector(m_vertexPositionData, positions);
+      appendVector(m_vertexNormalData, normals);
       appendVector(m_uvData, uvCoords);
 
-	    indexOffset += numIndices;
+      indexOffset += numIndices;
     }
 
 }
 
 //----------------------------------------------------------------------------------------
 void MeshConsolidator::getBatchInfoMap (
-		BatchInfoMap & batchInfoMap
+    BatchInfoMap & batchInfoMap
 ) const {
-	batchInfoMap = m_batchInfoMap;
+  batchInfoMap = m_batchInfoMap;
 }
 
 //----------------------------------------------------------------------------------------
 // Returns the starting memory location for vertex position data.
 const float * MeshConsolidator::getVertexPositionDataPtr() const {
-	return &(m_vertexPositionData[0].x);
+  return &(m_vertexPositionData[0].x);
 }
 
 //----------------------------------------------------------------------------------------
@@ -102,17 +102,17 @@ const float * MeshConsolidator::getUVDataPtr() const {
 //----------------------------------------------------------------------------------------
 // Returns the total number of bytes of all vertex position data.
 size_t MeshConsolidator::getNumVertexPositionBytes() const {
-	return m_vertexPositionData.size() * sizeof(vec3);
+  return m_vertexPositionData.size() * sizeof(vec3);
 }
 
 //----------------------------------------------------------------------------------------
 // Returns the total number of bytes of all vertex normal data.
 size_t MeshConsolidator::getNumVertexNormalBytes() const {
-	return m_vertexNormalData.size() * sizeof(vec3);
+  return m_vertexNormalData.size() * sizeof(vec3);
 }
 
 //----------------------------------------------------------------------------------------
 // Returns the total number of bytes of all vertex normal data.
 size_t MeshConsolidator::getNumUVBytes() const {
-	return m_uvData.size() * sizeof(vec2);
+  return m_uvData.size() * sizeof(vec2);
 }
